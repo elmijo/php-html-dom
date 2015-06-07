b7b29f0431d44b8334d59b4e6d8f88b25fc599d4
Jerry Anselmi <jerry.anselmi@gmail.com>
2015-06-07 01:20:45 -0430

    mas tests

--- tests/PHPHtmlDomLogTest.php
+++ tests/PHPHtmlDomLogTest.php
@@ -7,15 +7,15 @@ class PHPHtmlDomLogTest extends PHPUnit_Framework_TestCase
         $this->logger = new \PHPTools\PHPHtmlDom\Core\PHPHtmlDomLog;
         $this->assertInstanceOf('\PHPTools\PHPHtmlDom\Core\PHPHtmlDomLog', $this->logger);        
     }
-    public function testLogError()
-    {
-        $this->assertNull($this->logger->logError('E000',array('probando logError 1..')));
-        $this->assertNull($this->logger->logError('E001',array('probando logError 1.. 2..')));
-        $this->assertNull($this->logger->logError('E002',array('probando logError 1.. 2.. 3..')));
-        $this->assertNull($this->logger->logError('E003',array('probando logError 1.. 2.. 3.. 4..')));
-    }
-    public function testlogWarn()
-    {
-        $this->assertNull($this->logger->logWarn('W001',array('probando logWarn 1..')));
-    }
+    // public function testLogError()
+    // {
+    //     $this->assertNull($this->logger->logError('E000',array('probando logError 1..')));
+    //     $this->assertNull($this->logger->logError('E001',array('probando logError 1.. 2..')));
+    //     $this->assertNull($this->logger->logError('E002',array('probando logError 1.. 2.. 3..')));
+    //     $this->assertNull($this->logger->logError('E003',array('probando logError 1.. 2.. 3.. 4..')));
+    // }
+    // public function testlogWarn()
+    // {
+    //     $this->assertNull($this->logger->logWarn('W001',array('probando logWarn 1..')));
+    // }
 }
